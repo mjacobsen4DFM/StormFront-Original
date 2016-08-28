d765f93d6b01555dbf3d416d66b30e0c35333f52
Michael Jacobsen <mjacobsen@denverpost.com>
2016-08-27 21:33:48 -0600

    Better Redis logging
    Added levels to the logs: info, warning, error
    
    ~info: noisy messages of little or no importance
    ~warning: errors that do no cause critical failures (i.e. prevent
    delivery of required data)
    ~error: errors that cause critical failures

--- Client/src/main/java/com/DFM/StormFront/Client/Util/RedisLogUtil.java
+++ Client/src/main/java/com/DFM/StormFront/Client/Util/RedisLogUtil.java
@@ -10,43 +10,60 @@ import java.util.Date;
 
 public class RedisLogUtil {
     public static void logError(Exception e, RedisClient redisClient) {
-        String fullStackTrace = ExceptionUtil.getFullStackTrace(e);
-        logError(fullStackTrace, redisClient);
+        logError("", e, redisClient);
     }
 
     public static void logError(String msg, Exception e, RedisClient redisClient) {
         String fullStackTrace = ExceptionUtil.getFullStackTrace(e);
-        logError("", msg, fullStackTrace, redisClient);
+        logError(msg, fullStackTrace, redisClient);
     }
 
-    public static void logError(String tupleMsg, String friendlyMsg, String exceptionMsg, RedisClient redisClient) {
-        String strTuple = (!tupleMsg.equals("")) ? "Tuple(" + tupleMsg + ");" : tupleMsg;
+    private static void logError(String friendlyMsg, String exceptionMsg, RedisClient redisClient) {
+        String strLogMessage = compileMessage(friendlyMsg, exceptionMsg);
+        logError("Warning: " + strLogMessage.replace(");", ");\r\n"), redisClient);
+    }
+
+    private static void logError(String msg, RedisClient redisClient) {
+        log(msg, redisClient, "errors");
+    }
+
+    public static void logWarning(Exception e, RedisClient redisClient) {
+        logWarning("", e, redisClient);
+    }
+
+    public static void logWarning(String msg, Exception e, RedisClient redisClient) {
+        String fullStackTrace = ExceptionUtil.getFullStackTrace(e);
+        logWarning(msg, fullStackTrace, redisClient);
+    }
+
+    private static void logWarning(String friendlyMsg, String exceptionMsg, RedisClient redisClient) {
+        String strLogMessage = compileMessage(friendlyMsg, exceptionMsg);
+        logWarning("Warning: " + strLogMessage.replace(");", ");\r\n"), redisClient);
+    }
+
+    private static void logWarning(String msg, RedisClient redisClient) {
+        log(msg, redisClient, "warnings");
+    }
+
+    public static void logInfo(String msg, RedisClient redisClient) {
+        log(msg, redisClient, "info");
+    }
+
+    private static void log(String msg, RedisClient redisClient, String level) {
+        try {
+            msg = (msg == null) ? "No message to log." : msg;
+            LogUtil.log(msg);
+            String hostname = SystemUtil.getHostname();
+            String time = new Date().toString().replace(":", "-");
+            redisClient.set(level +":" + hostname + ":" + time, msg);
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static String compileMessage(String friendlyMsg, String exceptionMsg){
         String strMessage = (!friendlyMsg.equals("")) ? "Message(" + friendlyMsg + ");" : friendlyMsg;
         String strException = (!exceptionMsg.equals("")) ? "Exception(" + exceptionMsg + ");" : exceptionMsg;
-        String strLogMessage = strTuple + " " + strMessage + " " + strException;
-        logError("Error: " + strLogMessage.replace(");", ");\r\n"), redisClient);
-    }
-
-    public static void logError(String msg, RedisClient redisClient) {
-        try {
-            msg = (msg == null) ? "No error message to log." : msg;
-            LogUtil.log(msg);
-            String hostname = SystemUtil.getHostname();
-            String time = new Date().toString().replace(":", "-");
-            redisClient.set("errors:" + hostname + ":" + time, msg);
-        } catch (Exception ignored) {
-        }
-    }
-
-    public static void logFail(String msg, RedisClient redisClient) {
-        try {
-            msg = (msg == null) ? "No error message to log." : msg;
-            LogUtil.log(msg);
-            String hostname = SystemUtil.getHostname();
-            String time = new Date().toString().replace(":", "-");
-            redisClient.set("fails:" + hostname + ":" + time, msg);
-        } catch (Exception ignored) {
-        }
+        return strMessage + " " + strException;
     }
 
 }
