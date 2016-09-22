07b5c418d7af428ba4e7ecb50a1c5b3e6de006c6
Michael Jacobsen <mjacobsen@denverpost.com>
2016-09-21 22:11:54 -0600

    Lots of tweaks

--- Model/src/main/java/com/DFM/StormFront/Model/Storm/MsgTracker.java
+++ Model/src/main/java/com/DFM/StormFront/Model/Storm/MsgTracker.java
@@ -1,5 +1,8 @@
 package com.DFM.StormFront.Model.Storm;
 
+import com.DFM.StormFront.Util.DateTimeUtil;
+import com.DFM.StormFront.Util.LogUtil;
+
 /**
  * Created by Mick on 9/19/2016.
  */
@@ -7,20 +10,23 @@ public class MsgTracker {
     public String key = "";
     public long msStart = 0;
     public long msDelay = 0;
-    protected long completion = 0;
-    protected long duration = 0;
+    protected long msCompletion = 0;
+    protected long msDuration = 0;
     public String delim = "~";
 
     public MsgTracker(String key) {
-        if (!key.contains(this.delim)) {
+        if (key.contains(this.delim)) {
+            this.fromString(key);
+        } else {
             this.key = key;
             this.msStart = System.currentTimeMillis();
-        } else {
-            this.fromSting(key);
+            this.msDelay = 0;
+            this.msCompletion = 0;
+            this.msDuration = 0;
         }
     }
 
-    private void fromSting(String key) {
+    private void fromString(String key) {
         String[] msgArray = key.split(this.delim);
         this.key = msgArray[0];
         this.msStart = Long.parseLong(msgArray[1]);
@@ -35,30 +41,33 @@ public class MsgTracker {
     }
 
     public boolean Ready(){
-        return msStart < System.currentTimeMillis();
+        return msStart - 1000 < System.currentTimeMillis();
     }
 
     public boolean TooFast(Integer msDelay){
-        boolean bTooFast = this.getDuration() < msDelay;
+        boolean bTooFast = this.getMsDuration() < msDelay;
         if( bTooFast ){
-            this.msDelay = msDelay - this.getDuration();
-            this.NewStart(this.getCompletion() + this.msDelay);
+            this.msDelay = msDelay - this.getMsDuration();
+            LogUtil.log(String.format("Prev START: %s", DateTimeUtil.MilliSecondsToDateISO8601(this.msStart)));
+            LogUtil.log(String.format("Prev ACK: %s", DateTimeUtil.MilliSecondsToDateISO8601(this.getMsCompletion())));
+            this.NewStart(this.msStart + this.msDelay);
+            LogUtil.log(String.format("Next START: %s", DateTimeUtil.MilliSecondsToDateISO8601(this.msStart)));
         }
         return bTooFast;
     }
 
-    public long getCompletion(){
-        if ( this.completion == 0 ){
-            this.completion = System.currentTimeMillis();
+    public long getMsCompletion(){
+        if ( this.msCompletion == 0 ){
+            this.msCompletion = System.currentTimeMillis();
         }
-        return this.duration;
+        return this.msCompletion;
     }
 
-    public long getDuration(){
-        if ( this.duration == 0 ){
-            this.duration = this.getCompletion() - this.msStart;
+    public long getMsDuration(){
+        if ( this.msDuration == 0 ){
+            this.msDuration = this.getMsCompletion() - this.msStart;
         }
-        return this.duration;
+        return this.msDuration;
     }
 
     @Override
